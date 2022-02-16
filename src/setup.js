bf69864558e22a9c1eb44a39781dd67b4e7c538e
Tumi <thorvaldur.tumi@gmail.com>
2022-02-16 21:58:32 +0000

    laga drop.sql, þurfti að hafa röðina registration -> users -> events, vegna foreignkey

--- src/setup.js
+++ src/setup.js
@@ -1,31 +1,10 @@
 import { promises } from "fs";
-import faker from "faker";
 
 import { query, end } from "./lib/db.js";
 
-const SCHEMAFILE = "./sql/schema.sql";
 const DROPFILE = "./sql/drop.sql";
+const SCHEMAFILE = "./sql/schema.sql";
 const INSERT = "./sql/insert.sql";
-
-function createFakeData(n) {
-  const fake = [];
-
-  while (fake.length < n) {
-    const randomPastDate = new Date();
-    randomPastDate.setDate(randomPastDate.getDate() - 30);
-
-    let name = faker.name.findName();
-    fake.push({
-      name: name,
-      slug: name.replaceAll(" ", "-"),
-      description: faker.lorem.sentence(),
-      created: randomPastDate,
-      modified: faker.date.between(randomPastDate, new Date()),
-    });
-  }
-
-  return fake;
-}
 
 async function create() {
   const dropData = await promises.readFile(DROPFILE);
@@ -35,34 +14,7 @@ async function create() {
   await query(schemeData.toString("utf-8"));
 
   const insertData = await promises.readFile(INSERT);
-  await query(insertData.toString('utf-8'))
-
-  const fakes = createFakeData(1);
-
-  for (let i = 0; i < fakes.length; i += 1) {
-    const fake = fakes[i];
-
-    try {
-      // eslint-disable-next-line no-await-in-loop
-      await query(
-        `
-            INSERT INTO
-              events (name, slug, description, created, modified)
-            VALUES
-              ($1, $2, $3, $4, $5)`,
-        [fake.name, fake.slug, fake.description, fake.created, fake.modified]
-      );
-    } catch (e) {
-      console.error("Error inserting", e);
-      return;
-    }
-  }
-
-  try {
-    
-  } catch (error) {
-    
-  }
+  await query(insertData.toString('utf-8'));
 
   await end();
 
